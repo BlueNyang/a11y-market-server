8e5b3363c4848c79a55b9d5bcfd3b454d2c1443b
BlueNyang <dksrbxo@bluenyang.kr>
2025-12-05 13:11:49 +0900

    feat: add view table for CategoryRecommendations

--- sql/29.main-page-info.sql
+++ sql/29.main-page-info.sql
@@ -21,17 +21,11 @@ CREATE OR REPLACE VIEW view_monthly_popular_products AS
    ON p.category_id = cat.category_id
      LEFT JOIN product_images pi
    ON p.product_id = pi.product_id
-    WHERE o.created_at >= trunc(
+    WHERE o.created_at >= add_months(
          sysdate,
-         'MM'
+         -1
       )
-      AND o.created_at < add_months(
-      trunc(
-         sysdate,
-         'MM'
-      ),
-      1
-   )
+      AND o.created_at < sysdate
     GROUP BY p.product_id,
              p.product_name,
              p.product_price,
@@ -40,3 +34,42 @@ CREATE OR REPLACE VIEW view_monthly_popular_products AS
              cat.category_name,
              p.seller_id
     ORDER BY monthly_sales_volume DESC;
+
+CREATE OR REPLACE VIEW view_category_recommendations AS
+   WITH category_tree (
+      root_id,
+      root_name,
+      leaf_id
+   ) AS (
+      SELECT category_id,
+             category_name,
+             category_id
+        FROM categories
+       WHERE parent_cat_id IS NULL
+      UNION ALL
+      SELECT p.root_id,
+             p.root_name,
+             cat.category_id
+        FROM categories cat
+        JOIN category_tree p
+      ON cat.parent_cat_id = p.leaf_id
+   ),ranked_products AS (
+      SELECT ct.root_id,
+             ct.root_name,
+             vp.product_id,
+             vp.product_name,
+             vp.product_price,
+             vp.product_image_url,
+             vp.monthly_sales_volume,
+             ROW_NUMBER()
+             OVER(PARTITION BY ct.root_id
+                  ORDER BY vp.monthly_sales_volume DESC,
+                           vp.product_id DESC
+             ) AS rn
+        FROM view_monthly_popular_products vp
+        JOIN category_tree ct
+      ON vp.category_id = ct.leaf_id
+   )
+   SELECT *
+     FROM ranked_products
+    WHERE rn <= 4;
